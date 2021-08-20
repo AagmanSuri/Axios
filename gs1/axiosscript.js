d027f611eabfd23ee31583bc87065d522b8999f9
Aagman Suri <aagmansuri96@gmail.com>
2021-08-20 18:02:19 +0530

    errror handeling in js

--- gs1/axiosscript.js
+++ gs1/axiosscript.js
@@ -1,15 +1,27 @@
 btn = document.getElementById("btn");
 const makerequest = () => {
   console.log("clicked");
-  config = {
-    method: "get",
-    url: "data.txt",
-  };
+  //   config = {
+  //     method: "get",
+  //     url: "data.txt",
+  //   };
 
-  const promiseObj = axios(config);
-  promiseObj.then((res) => {
-    console.log(res);
-  });
+  //   const promiseObj = axios(config);
+  //   promiseObj.then((res) => {
+  //     console.log(res);
+  //     console.log(res.data);
+  //   });
+
+  //   axios(config).then((res) => {
+  //     console.log(res);
+  //   });
+
+  axios
+    .get("data1.txt")
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => console.log(error));
 };
 
 btn.addEventListener("click", makerequest);
